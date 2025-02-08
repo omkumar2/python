5643da4f5e235b9342b748e7425644cb4834d777
omkumar2 <omkumariitbomboy@gmail.com>
2025-02-08 17:31:54 +0530

    first commit

---
+++ python 1/fristcode.py
@@ -0,0 +1,6 @@
+x='pytHoN sTrIng MEthOdS'
+print(x.lower()) #python string methods
+print(x.upper()) #PYTHON STRING METHODS
+print(x.capitalize()) #Python string methods
+print(x.title()) #Python String Methods
+print(x.swapcase()) #PYThOn StRiNG meTHoDs
