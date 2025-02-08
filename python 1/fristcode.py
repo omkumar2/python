ddaca6c18f2349a0fec8626bf2a5c7932e1e5ec9
omkumar2 <omkumariitbomboy@gmail.com>
2025-02-08 17:40:05 +0530

    new

--- python 1/fristcode.py
+++ python 1/fristcode.py
@@ -4,3 +4,16 @@ print(x.upper()) #PYTHON STRING METHODS
 print(x.capitalize()) #Python string methods
 print(x.title()) #Python String Methods
 print(x.swapcase()) #PYThOn StRiNG meTHoDs
+
+x = 'python'
+print(x.islower()) #true
+x = 'Python'
+print(x.islower()) #false
+x = 'PYTHON'
+print(x.isupper()) #true
+x = 'PyThOn'
+print(x.isupper()) #false
+x = 'Python String Methods'
+print(x.istitle()) #true
+x = 'Python string methods'
+print(x.istitle()) #false
